a7947682bd6fefcf3f2da030df5298b337518f87
moncefplastin07 <moncefplastin07@gmail.com>
2022-07-25 11:11:41 +0100

    Add an alert about links not working

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,9 @@ import { h } from "preact";
 import { PageProps, Handlers } from "$fresh/server.ts";
 import { Head } from "$fresh/src/runtime/head.ts";
 import { tw } from "@twind";
+import Layout from "../components/Layout.tsx";
 import RegionsList from "../components/RegionsList.tsx";
+import Alert from "../components/Alert.tsx";
 
 export const handler: Handlers = {
   async GET(req, ctx) {
@@ -15,26 +17,23 @@ export const handler: Handlers = {
 };
 export default function Home({data}:PageProps) {
   return (
+  <Layout title="DZGitrs - The most active GitHub By geographic region">
     <div
-        className="dark:bg-black dark:text-white flex flex-col items-center justify-center min-h-screen py-2"
-      >
-        <Head>
-          <title>DZGitrs - The most active GitHub By geographic region</title>
-          <meta name="viewport" content="width=device-width"></meta>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        {/* {alert ? <Alert {...alert} /> : ""} */}
-        <main
-          className={tw`flex flex-col items-center justify-center flex-1 text-center`}
+          className="dark:bg-black dark:text-white flex flex-col items-center justify-center min-h-screen py-2"
         >
-          <h1 className={tw`text-4xl font-bold lg:text-6xl`}>
-            Welcome to{" "}
-            <span className={tw`text-green-600`}>
-              DZGitrs!
-            </span>
-          </h1>
-          <RegionsList listOfRegions={data}/>
-        </main>
-      </div>
+          <main
+            className={tw`flex flex-col items-center justify-center flex-1 text-center`}
+          >
+            <h1 className={tw`text-4xl font-bold lg:text-6xl`}>
+              Welcome to{" "}
+              <span className={tw`text-green-600`}>
+                DZGitrs!
+              </span>
+            </h1>
+            <Alert title="Warning: " body="Some of the following links may not work for source reasons"></Alert>
+            <RegionsList listOfRegions={data}/>
+          </main>
+        </div>
+      </Layout>
   );
 }
