f07c4b79d6cc85a01dbf70164278150ecc047bd5
moncefplastin07 <moncefplastin07@gmail.com>
2022-07-25 11:09:24 +0100

    style: add tw styleing

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,16 +1,14 @@
-
-// support jsx on deno deploy
-/** @jsxImportSource https://esm.sh/react@18.2.0 */
-
-import { Link } from "aleph/react";
+/** @jsx h */
+import { tw } from "@twind";
+import { h } from "preact";
 
 export default function Alert({ title, body }: any) {
   return (
     <div
-      className="p-3 m-5 text-red-500 bg-red-50 rounded-md fixed left-0 top-10"
+      className={tw`p-3 m-5 text-red-500 bg-red-50 rounded-md`}
     >
       <p>
-        <span className="font-bold">{title}</span>
+        <span className={tw`font-bold`}>{title}</span>
         <span>{body}</span>
       </p>
     </div>
